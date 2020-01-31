33ed215b2e5cbbc88a2e31ba79fe592b1f6784ce
Coding <codingkobold@gmail.com>
2020-01-31 20:52:41 +0100

    Added basic game boundaries

--- src/gameScene.ts
+++ src/gameScene.ts
@@ -17,7 +17,24 @@ export class GameScene extends Phaser.Scene {
     }
 
     create(): void {
-       
+        let graphics = this.add.graphics();
+
+        let outerArea: Phaser.Geom.Rectangle = new Phaser.Geom.Rectangle(0, 0, 144, 576);
+        let workshopArea: Phaser.Geom.Rectangle = new Phaser.Geom.Rectangle(144, 0, 736, 576);
+        let itemsArea: Phaser.Geom.Rectangle = new Phaser.Geom.Rectangle(880, 0, 464, 720);
+        let dialogArea: Phaser.Geom.Rectangle = new Phaser.Geom.Rectangle(0, 576, 880, 144);
+        
+        graphics.fillStyle(0x00ff00);
+        graphics.fillRectShape(outerArea);
+
+        graphics.fillStyle(0xffff00);
+        graphics.fillRectShape(workshopArea);
+
+        graphics.fillStyle(0x0000ff);
+        graphics.fillRectShape(itemsArea);
+
+        graphics.fillStyle(0x000000);
+        graphics.fillRectShape(dialogArea);
     }
 
     update(): void {
