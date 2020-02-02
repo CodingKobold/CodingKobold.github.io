bd34b4ad7940c58292a4493f8898d4e175a6a7b3
ExpressionSigh <25393492+ExpressionSigh@users.noreply.github.com>
2020-02-02 12:22:09 +0100

    fajrant

--- src/scoreScene.ts
+++ src/scoreScene.ts
@@ -3,6 +3,7 @@ import "phaser";
 export class ScoreScene extends Phaser.Scene {
   score: number;
   result: Phaser.GameObjects.Text;
+  scoreText: Phaser.GameObjects.Text;
   hint: Phaser.GameObjects.Text;
 
   constructor() {
@@ -15,22 +16,32 @@ export class ScoreScene extends Phaser.Scene {
     this.score = params.score;
   }
 
+  preload(){
+      this.load.image('brigade-logo', 'images/brigade-logo.png');
+      this.load.image('fajrant', 'images/fajrant.png');
+  }
+
   create(): void {
-    var resultText: string = 'Your score is ' + this.score + '!';
-    
-    this.result = this.add.text(200, 250, resultText,
-      { font: '48px Arial Bold', fill: 'red' });
+    this.add.image(675, 100, 'brigade-logo').setDisplaySize(300,150);
+    this.add.image(675, 275, 'fajrant').setDisplaySize(700,200);
 
-    var hintText: string = "Click to restart";
-    
-    this.hint = this.add.text(300, 350, hintText,
-      { 
-        font: '24px Arial Bold',
-        fill: '#FBFBAC' 
-      });
+    if (this.score > 0 ){
+      this.add.text(420, 400, "Zarobiłeś: ", { font: '48px Consolas', fill: '#FBFBAC'});
+      this.add.text(775, 400, this.score + "PLN", { font: '48px Consolas', fill: 'green'});
+      this.add.text(440, 475, "Można iść na flaszeczkę! :D", { font: '32px Consolas', fill: '#FBFBAC'});
+    }
+    if (this.score <= 0 ){
+      this.add.text(420, 400, "Straciłeś: ", { font: '48px Consolas', fill: '#FBFBAC'});
+      this.add.text(775, 400, Math.abs(this.score) + "PLN", { font: '48px Consolas', fill: 'red'});
+      this.add.text(440, 475, "Dzisiaj bez flaszeczki :C", { font: '32px Consolas', fill: '#FBFBAC'});
+    }
 
-    // this.input.on('pointerdown', function (/*pointer*/) {
-    //   this.scene.start("WelcomeScene");
-    // }, this);
+    this.add.text(150, 600, "Naciśnij spacje, aby zacząć kolejny dzień",  { font: '48px Consolas', fill: '#FBFBAC' });
+
+    var keyObj = this.input.keyboard.addKey('space');  // Get key object
+
+    keyObj.on('down', function(event) { 
+        this.scene.start("GameScene");
+    }, this);
   }
 };
--- src/welcomeScene.ts
+++ src/welcomeScene.ts
@@ -16,11 +16,12 @@ export class WelcomeScene extends Phaser.Scene {
     }
 
     create(): void {    
-
         this.add.image(675, 250, 'brigade-logo').setDisplaySize(900,400);
         this.add.image(675, 575, 'nie-ma-problemu').setDisplaySize(1400,600);
 
-        this.input.on('pointerdown', function (/*pointer*/) {
+        var keyObj = this.input.keyboard.addKey('space');  // Get key object
+
+        keyObj.on('down', function(event) { 
             this.scene.start("GameScene");
         }, this);
     }
